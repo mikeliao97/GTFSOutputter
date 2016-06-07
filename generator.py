62e2a71e65ca8075bd3389babcae23dc9b14a8b7
Mike Liao <mikeliao97@gmail.com>
2016-06-07 13:29:33 -0700

    Finished Table 1 Agency for BART

--- generator.py
+++ generator.py
@@ -11,9 +11,14 @@ import sqlalchemy
 from sqlalchemy.types import String
 
 #Goal: Use Bart Information to Produce the Task 1 For Bart
-# Static Feed
-# ---- Task 1 ----
-# Agency
+
+def optional_field(index, column, dataframe, default='N/A'):
+    row = dataframe.iloc[index]
+    return row[column] if (column in dataframe.columns and not pd.isnull(row[column])) else default
+
+
+tables = {}
+# Table1: Agency
 # int agency_id -> 'agency_id' int(10) unsigned
 # required string agency_name  -> 'agency_name' varchar(255)
 # required string agency_url -> 'agency_url' varchar(255)
@@ -23,37 +28,90 @@ from sqlalchemy.types import String
 # required string agency_timezone -> 'timezone_name' varchar(45)
 # PRIMARY KEY ('agency_id')
 # KEY ('agency_timezone')
-
 try:
-    df = pd.read_csv("agencies/bart/agency.txt")
+    agency_df = pd.read_csv("agencies/bart/agency.txt")
 except Exception as e:
     print os.getcwd()
     print e
 
+columns = ['agency_id', 'agency_name', 'agency_url', 'agency_timezone', 'agency_lang',
+           'agency_phone', 'timezone_name']
+tables['Agency'] = pd.DataFrame(index=np.r_[0:len(agency_df.index)], columns=columns)
+for i, row in agency_df.iterrows():
+    new_row = tables["Agency"].loc[i] #instantiate a NEW ROW
 
-#what is the point of this?
-#what you need to do is to collect information, put it in df and then write it into a new table, then
-#write it to the SQL DATABASE
-# df.to_sql(con=db, flavor='mysql', index_label="agency_id", name="new_database", if_exists="replace")
-
-tables = {}
-columns = ['agency_id', 'agency_name', 'agency_url', 'agency_timezone', 'agency_lang', 'agency_phone', 'timezone_name', 'whatever']
-tables['Agency'] = pd.DataFrame(index=np.r_[0:len(df.index)], columns=columns)
-for i, row in df.iterrows():
-    new_row = tables["Agency"].loc[i]
-    new_row['agency_id'] = 10
+    new_row['agency_id'] = 1
     new_row['agency_name'] = row['agency_name']
     new_row['agency_url'] = row['agency_url']
     timezone = pytz.timezone(row['agency_timezone'])
-    # new_row['agency_lang'] = optional_field(i, 'agency_lang', static_feed['agency'])
-    # new_row['agency_phone'] = optional_field(i, 'agency_phone', static_feed['agency'])
+    # WHAT IS DIFFERENCE BETWEEN AGENCY TIMEZONE and timezone_name???
+    # new_row['agency_timezone'] = timezone
+    new_row['agency_lang'] = optional_field(i, 'agency_lang', agency_df)
+    new_row['agency_phone'] = optional_field(i, 'agency_phone', agency_df)
     new_row['timezone_name'] = row['agency_timezone']
 
 #Now use tables['Agency'] and write it to the database
-print tables['Agency']
+print tables['Agency'].dtypes
 another_dataframe = tables['Agency']
 db = MySQLdb.connect(host="localhost", user="root",
                      passwd="berkeleypath", db="TrafficTransit")
-another_dataframe.to_sql(con=db, flavor='mysql', name="test_database", if_exists="replace")
+another_dataframe.to_sql(con=db, flavor='mysql', name="Agency", if_exists="replace",
+                         dtype = {'agency_id' : "INT(10) NOT NULL",
+                                  'agency_name': "VARCHAR(255) NOT NULL",
+                                   'agency_url': "VARCHAR(255) NOT NULL",
+                                    # 'agency_timezone': "smallint(6) NOT NULL",
+                                    'agency_lang': "VARCHAR(255) NOT NULL",
+                                    'agency_phone': "VARCHAR(255) NOT NULL",
+                                    'timezone_name': "VARCHAR(45) NOT NULL"})
+
+
+#Table 2: Routes
+# int agency_id -> 'agency_id' int(10) unsigned
+# required string route_short_name -> 'route_short_name' varchar(255)
+# optional int direction_id -> 'route_dir' int(10) unsigned
+# required int route_type -> 'route_type' int(10) unsigned
+# required string route_long_name -> 'route_long_name' varchar(255) default 'N/A'
+# optional string route_desc -> 'route_desc' varchar(255) default 'N/A'
+# optional string route_url -> 'route_url' varchar(255) default 'N/A'
+# optional string route_color -> 'route_color' varchar(255) default 'FFFFFF',
+# optional string route_text_color -> 'route_text_color' varchar(255) default '000000'
+# required string route_id -> 'route_id' varchar(255) default '000000'
+# zipfile MD5 -> 'version' varchar(255)
+try:
+    agency_df = pd.read_csv("agencies/bart/agency.txt")
+except Exception as e:
+    print os.getcwd()
+    print e
+
+columns = ['agency_id', 'agency_name', 'agency_url', 'agency_timezone', 'agency_lang',
+           'agency_phone', 'timezone_name']
+tables['Agency'] = pd.DataFrame(index=np.r_[0:len(agency_df.index)], columns=columns)
+for i, row in agency_df.iterrows():
+    new_row = tables["Agency"].loc[i] #instantiate a NEW ROW
+
+    new_row['agency_id'] = 1
+    new_row['agency_name'] = row['agency_name']
+    new_row['agency_url'] = row['agency_url']
+    timezone = pytz.timezone(row['agency_timezone'])
+    # WHAT IS DIFFERENCE BETWEEN AGENCY TIMEZONE and timezone_name???
+    # new_row['agency_timezone'] = timezone
+    new_row['agency_lang'] = optional_field(i, 'agency_lang', agency_df)
+    new_row['agency_phone'] = optional_field(i, 'agency_phone', agency_df)
+    new_row['timezone_name'] = row['agency_timezone']
+
+#Now use tables['Agency'] and write it to the database
+print tables['Agency'].dtypes
+another_dataframe = tables['Agency']
+db = MySQLdb.connect(host="localhost", user="root",
+                     passwd="berkeleypath", db="TrafficTransit")
+another_dataframe.to_sql(con=db, flavor='mysql', name="Agency", if_exists="replace",
+                         dtype = {'agency_id' : "INT(10) NOT NULL",
+                                  'agency_name': "VARCHAR(255) NOT NULL",
+                                   'agency_url': "VARCHAR(255) NOT NULL",
+                                    # 'agency_timezone': "smallint(6) NOT NULL",
+                                    'agency_lang': "VARCHAR(255) NOT NULL",
+                                    'agency_phone': "VARCHAR(255) NOT NULL",
+                                    'timezone_name': "VARCHAR(45) NOT NULL"})
+
 
 
